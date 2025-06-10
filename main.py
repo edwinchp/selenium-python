ef5d4784e03fdbd050ceaab2c17af88348dc191f
edwinchp <chipineiro@gmail.com>
2025-06-10 17:24:37 -0600

    Automate login and assert login url

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 time.sleep(5)
 
-driver.get("https://www.google.com")
+driver.get("https://practicetestautomation.com/practice-test-login/")
 
 time.sleep(5)
+
+username_locator = driver.find_element(By.ID, "username")
+username_locator.send_keys("student")
+
+password_locator = driver.find_element(By.NAME, "password")
+password_locator.send_keys("Password123")
+
+submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+submit_button_locator.click()
+
+actual_url = driver.current_url
+assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+
+
 
 driver.close()
