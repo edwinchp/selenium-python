b41fc2be84ad60bcd18f2adfb162735f21d273f1
edwinchp <chipineiro@gmail.com>
2025-06-10 17:12:34 -0600

    Update gitignore

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import time
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
 
+driver = webdriver.Chrome()
+time.sleep(5)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+driver.get("https://www.google.com")
 
+time.sleep(5)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+driver.close()
