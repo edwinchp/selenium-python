11f93c4105458727ec1098500c3270674c269c92
edwinchp <chipineiro@gmail.com>
2025-06-12 09:27:33 -0600

    Move files to tests

---
+++ tests/test_login_page.py
@@ -0,0 +1,40 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestPositivesScenarios:
+
+    @pytest.mark.login
+    def test_positive_login(self):
+        driver = webdriver.Chrome()
+        time.sleep(5)
+
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        time.sleep(5)
+
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
+
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("Password123")
+
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_button_locator.click()
+
+        actual_url = driver.current_url
+        assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+
+        text_locator = driver.find_element(By.TAG_NAME, "h1")
+        actual_text = text_locator.text
+        assert actual_text == "Logged In Successfully"
+
+        log_out_button_locator = driver.find_element(By.LINK_TEXT, "Log out")
+        assert log_out_button_locator.is_displayed()
+
+        driver.close()
