90bacb68aa0380a4124126a80904fcd0ad395466
edwinchp <chipineiro@gmail.com>
2025-06-13 14:46:01 -0600

    Parametrize negative test

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -8,24 +8,29 @@ class TestPositivesScenarios:
 
     @pytest.mark.login
     @pytest.mark.negative
-    def test_negative_login(self, driver):
+    @pytest.mark.parametrize("username, password, expected_error_message",
+                             [("incorrectUser", "incorrectPassword", "Your username is invalid!"),
+                              ("student", "incorrectPassword", "Your password is invalid!")])
+    def test_negative_login(self, driver, username, password, expected_error_message):
 
         driver.get("https://practicetestautomation.com/practice-test-login/")
 
         time.sleep(2)
 
         username_locator = driver.find_element(By.ID, "username")
-        username_locator.send_keys("student")
+        username_locator.send_keys(username)
 
         password_locator = driver.find_element(By.NAME, "password")
-        password_locator.send_keys("wrongpassword")
+        password_locator.send_keys(password)
 
         submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
         submit_button_locator.click()
+
+        time.sleep(2)
 
         actual_url = driver.current_url
         assert actual_url == "https://practicetestautomation.com/practice-test-login/"
 
         error_text_locator = driver.find_element(By.ID, "error")
         actual_text = error_text_locator.text
-        assert actual_text == "Your password is invalid!"
+        assert actual_text == expected_error_message
