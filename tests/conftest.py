94124ed40fb9c5ddf28f1ac20c248c8d6b70238e
edwinchp <chipineiro@gmail.com>
2025-06-13 15:25:54 -0600

    Support firefox and chrome browser, include addoption to select in console which one to run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,26 @@
 import pytest
+from pygments.lexer import default
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture()
-def driver():
-    print("Creating Chrome Driver")
-    my_driver = webdriver.Chrome()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} Driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        my_driver =  webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise TypeError(f"Browser not supported: {browser}")
     yield my_driver
-    print("Closing Chrome Driver")
+    print(f"Closing {browser} Driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="Browser to execute tests (chrome or firefox)"
+    )
