0a680f9a5630cef06cfce7305f4ba14f4761223b
edwinchp <chipineiro@gmail.com>
2025-06-16 15:19:34 -0600

    Change the tuple thing

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -1,7 +1,7 @@
 from selenium.common import NoSuchElementException
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -13,26 +13,26 @@ class BasePage:
     def _open(self, url: str):
         self._driver.get(url)
 
-    def _find(self, locator: tuple) -> WebElement:
+    def _find(self, locator: tuple[str, str]) -> WebElement:
         return self._driver.find_element(*locator)
 
-    def _type(self, locator: tuple, text: str, time: int = 30):
+    def _type(self, locator: tuple[str, str], text: str, time: int = 30):
         self._wait_until_element_is_visible(locator, time)
         self._find(locator).send_keys(text)
 
-    def _click(self, locator: tuple, time: int = 30):
+    def _click(self, locator: tuple[str, str], time: int = 30):
         self._wait_until_element_is_visible(locator, time)
         self._find(locator).click()
 
-    def _wait_until_element_is_visible(self, locator: tuple, time: int = 30):
+    def _wait_until_element_is_visible(self, locator: tuple[str, str], time: int = 30):
         wait = WebDriverWait(self._driver, time)
-        wait.until(expected_conditions.visibility_of_element_located(*locator))
+        wait.until(ec.visibility_of_element_located(locator))
 
-    def _get_text(self, locator: tuple, time: int = 30) -> str:
+    def _get_text(self, locator: tuple[str, str], time: int = 30) -> str:
         self._wait_until_element_is_visible(locator, time)
         return self._find(locator).text
 
-    def _is_displayed(self, locator: tuple) -> bool:
+    def _is_displayed(self, locator: tuple[str, str]) -> bool:
         try:
             return self._find(locator).is_displayed()
         except NoSuchElementException:
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -3,11 +3,25 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
+from page_objects.logged_in_successfully_page import LoggedInSuccessfullyPage
+from page_objects.login_page import LoginPage
+
 
 class TestPositivesScenarios:
 
     @pytest.mark.login
     def test_positive_login(self, driver):
+
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.execute_login("student", "Password123")
+        logged_in_page = LoggedInSuccessfullyPage(driver)
+        assert logged_in_page.expected_url == logged_in_page.current_url, "Actual URL is not the same as expected."
+        assert logged_in_page.header == "Logged In Successfully", "Header is not expected"
+        assert logged_in_page.is_logout_button_displayed(), "Logout button should be visible"
+
+
+
 
         driver.get("https://practicetestautomation.com/practice-test-login/")
 
