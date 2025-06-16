d70a5876d2698e90ee29bb030270213b89848feb
edwinchp <chipineiro@gmail.com>
2025-06-16 15:42:07 -0600

    Fix negative test

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -11,6 +11,7 @@ class LoginPage(BasePage):
     __username_field = (By.ID, "username")
     __password_field = (By.NAME, "password")
     __submit_button = (By.XPATH, "//button[@class='btn']")
+    __error_message = (By.ID, "error")
 
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
@@ -22,3 +23,11 @@ class LoginPage(BasePage):
         super()._type(self.__username_field, username)
         super()._type(self.__password_field, password)
         super()._click(self.__submit_button)
+
+    @property
+    def error_message(self) -> str:
+        return super()._get_text(self.__error_message)
+
+    @property
+    def current_url(self) -> str:
+        return super().current_url
--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -3,6 +3,8 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
+from page_objects.login_page import LoginPage
+
 
 class TestPositivesScenarios:
 
@@ -13,20 +15,9 @@ class TestPositivesScenarios:
                               ("student", "incorrectPassword", "Your password is invalid!")])
     def test_negative_login(self, driver, username, password, expected_error_message):
 
-        driver.get("https://practicetestautomation.com/practice-test-login/")
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.execute_login(username, password)
 
-        username_locator = driver.find_element(By.ID, "username")
-        username_locator.send_keys(username)
-
-        password_locator = driver.find_element(By.NAME, "password")
-        password_locator.send_keys(password)
-
-        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
-        submit_button_locator.click()
-
-        actual_url = driver.current_url
-        assert actual_url == "https://practicetestautomation.com/practice-test-login/"
-
-        error_text_locator = driver.find_element(By.ID, "error")
-        actual_text = error_text_locator.text
-        assert actual_text == expected_error_message
+        assert login_page.current_url == "https://practicetestautomation.com/practice-test-login/", "Actual URL is not expected"
+        assert login_page.error_message == expected_error_message,  "Error message is not expected"
