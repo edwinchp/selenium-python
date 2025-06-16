1bfc92e3dcb5ba73d0284c4c10a5ea232a165c77
edwinchp <chipineiro@gmail.com>
2025-06-16 16:19:37 -0600

    Add init tests and page_objects

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -19,27 +19,3 @@ class TestPositivesScenarios:
         assert logged_in_page.expected_url == logged_in_page.current_url, "Actual URL is not the same as expected."
         assert logged_in_page.header == "Logged In Successfully", "Header is not expected"
         assert logged_in_page.is_logout_button_displayed(), "Logout button should be visible"
-
-
-
-
-        driver.get("https://practicetestautomation.com/practice-test-login/")
-
-        username_locator = driver.find_element(By.ID, "username")
-        username_locator.send_keys("student")
-
-        password_locator = driver.find_element(By.NAME, "password")
-        password_locator.send_keys("Password123")
-
-        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
-        submit_button_locator.click()
-
-        actual_url = driver.current_url
-        assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
-
-        text_locator = driver.find_element(By.TAG_NAME, "h1")
-        actual_text = text_locator.text
-        assert actual_text == "Logged In Successfully"
-
-        log_out_button_locator = driver.find_element(By.LINK_TEXT, "Log out")
-        assert log_out_button_locator.is_displayed()
